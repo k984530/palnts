bda4c1a103dca826bb81c68cf789195920c1befe
seon <kso323@yonsei.ac.kr>
2023-11-10 13:20:26 +0900

    [TEMP]

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:plants/Screens/StoreScreen.dart';
-=======
 import 'package:get/route_manager.dart';
 import 'package:kakao_map_plugin/kakao_map_plugin.dart';
 import 'package:plants/Screens/ChatScreen.dart';
 import 'package:plants/Screens/MapScreen.dart';
 import 'package:plants/Screens/PlantProfileScreen.dart';
->>>>>>> cdd8f3bd5c1b67e90141b0a6239cf8793ca282d7
+import 'package:plants/Screens/StoreScreen.dart';
 
 void main() {
   AuthRepository.initialize(appKey: '17eadd0cc722ec44323a286f5534d4b1');
@@ -27,17 +24,13 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-<<<<<<< HEAD
       home: StoreScreen(),
-=======
-      home: PlantProfileScreen(),
       getPages: [
         GetPage(
           name: '/Chat',
           page: () => ChatScreen(),
         ),
       ],
->>>>>>> cdd8f3bd5c1b67e90141b0a6239cf8793ca282d7
     );
   }
 }
