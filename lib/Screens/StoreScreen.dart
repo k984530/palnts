a17678fae4eb458390107508c12fa754714b29c4
seon <kso323@yonsei.ac.kr>
2023-11-10 11:07:20 +0900

    [ADD] StoreScreen

---
+++ lib/Screens/StoreScreen.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
+class StoreScreen extends StatelessWidget {
+  const StoreScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        flexibleSpace: Container(
+          margin: EdgeInsets.only(bottom: 10),
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 40,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                alignment: Alignment.bottomCenter,
+                image: AssetImage(
+                  'images/logo2.png',
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "플랜테리아 카페 이름",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                color: Colors.brown,
+              ),
+            ),
+            Container(
+              height: 250,
+              margin: EdgeInsets.all(
+                10,
+              ),
+              child: Image.asset(
+                'images/store1_1.jpg',
+              ),
+            ),
+            Container(
+              child: RatingBar.builder(
+                initialRating: 0,
+                minRating: 0,
+                direction: Axis.horizontal,
+                allowHalfRating: true,
+                itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                itemBuilder: (context, _) => Icon(
+                  Icons.star,
+                  color: Colors.lightGreen,
+                ),
+                onRatingUpdate: (rating) {
+                  print(rating);
+                },
+              ),
+            ),
+            Container(
+              height: 400,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    for (int i = 0; i < 4; i++)
+                      Container(
+                        height: 80,
+                        margin: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Colors.brown[(i + 1) % 9 * 100],
+                          borderRadius: BorderRadius.circular(
+                            10,
+                          ),
+                        ),
+                      )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
