4bd9473f6ed22e15189d1bc496e2aaad50146344
won <alyduho984530@gmail.com>
2023-11-11 00:54:59 +0900

    save

--- lib/Screens/MapScreen.dart
+++ lib/Screens/MapScreen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:get/get.dart';
+import 'package:get/get_navigation/src/routes/default_transitions.dart';
 import 'package:get/route_manager.dart';
 import 'package:kakao_map_plugin/kakao_map_plugin.dart';
 
@@ -11,24 +15,19 @@ class MapScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final DraggableScrollableController bottomController =
         DraggableScrollableController();
+
     return Scaffold(
       body: Stack(
-        children: [
+        children: <Widget>[
           KakaoMap(
             onMapCreated: (controller) async {
               mapController = controller;
               // mapController.addMarker(markers: [Marker(latLng: )])
-
               mapController.addMarker(
                 markers: [
                   Marker(
-                    markerId: 'test',
-                    latLng: LatLng(33.44986152248232, 126.57050723840896),
-                  ),
-                  Marker(
-                    markerId: 'test2',
-                    latLng: LatLng(33.450560189752, 126.57133888182207),
-                  ),
+                      markerId: 'test',
+                      latLng: LatLng(33.44986152248232, 126.57050723840896)),
                 ],
               );
             },
@@ -37,89 +36,173 @@ class MapScreen extends StatelessWidget {
               print(latLng);
             },
           ),
-          Positioned(
-            bottom: 70,
-            right: 30,
-            child: GestureDetector(
-              onTap: () {
-                Get.toNamed('/Resister');
-              },
-              child: Container(
-                width: 60,
-                height: 60,
-                padding: EdgeInsets.all(12),
-                decoration: BoxDecoration(
-                  color: Colors.green[300],
-                  borderRadius: BorderRadius.circular(30),
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 1,
-                      spreadRadius: 1,
-                      offset: Offset(4, 4),
-                      color: Colors.grey,
-                    )
-                  ],
-                ),
-                child: Image.asset(
-                  'images/plants.png',
-                ),
-              ),
-            ),
-          ),
           SizedBox.expand(
             child: DraggableScrollableSheet(
               minChildSize: 0.1,
-              maxChildSize: 0.80,
-              initialChildSize: 0.2,
-              builder:
-                  (BuildContext context, ScrollController scrollController) {
-                return SingleChildScrollView(
-                  // Consuming the scrollController provided
-                  controller: scrollController,
-                  child: Container(
-                    margin: EdgeInsets.only(
-                        top: 0.05 * MediaQuery.of(context).size.height),
-                    height: 0.75 * MediaQuery.of(context).size.height,
-
-                    // Generic Designing of the sheet
-                    decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.only(
-                        topRight: Radius.circular(30.0),
-                        topLeft: Radius.circular(30.0),
+              initialChildSize: 0.25,
+              controller: bottomController,
+              builder: (context, scrollController) {
+                return Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, -10),
                       ),
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: 5.0,
-                          spreadRadius: 20.0,
-                          offset: const Offset(0.0, 5.0),
-                          color: Colors.black.withOpacity(0.1),
-                        )
-                      ],
-                      color: Colors.white,
+                    ],
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(40),
                     ),
-                    // Contents of the sheet
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(height: 20),
-                        Row(
-                          children: [
-                            const Spacer(),
-                            Container(
-                              height: 4,
-                              width: 60,
+                  ),
+                  child: Stack(
+                    children: [
+                      ListView(
+                        controller: scrollController,
+                        children: <Widget>[
+                          ListTile(
+                            title: Text(
+                              "가게 1",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w700, fontSize: 22),
+                            ),
+                            trailing: Container(
+                              alignment: Alignment.bottomRight,
+                              width: 150,
+                              height: 150,
                               decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(15.0),
-                                color: Colors.grey,
+                                image: DecorationImage(
+                                  image: AssetImage('images/store2.jpeg'),
+                                ),
                               ),
                             ),
-                            const Spacer(),
-                          ],
+                            subtitle: RatingBar.builder(
+                              itemSize: 15,
+                              initialRating: 4.5,
+                              minRating: 1,
+                              direction: Axis.horizontal,
+                              allowHalfRating: true,
+                              itemCount: 5,
+                              itemBuilder: (context, _) => Icon(
+                                Icons.star,
+                                color: const Color.fromARGB(255, 26, 182, 107),
+                              ),
+                              onRatingUpdate: (double value) {},
+                            ),
+                          ),
+                          Divider(color: Colors.grey),
+                          ListTile(
+                            title: Text(
+                              "가게 2",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w700, fontSize: 22),
+                            ),
+                            trailing: Container(
+                              alignment: Alignment.bottomRight,
+                              width: 150,
+                              height: 150,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage('images/store3.jpeg'),
+                                ),
+                              ),
+                            ),
+                            subtitle: RatingBar.builder(
+                              itemSize: 15,
+                              initialRating: 3,
+                              minRating: 1,
+                              direction: Axis.horizontal,
+                              allowHalfRating: true,
+                              itemCount: 5,
+                              itemBuilder: (context, _) => Icon(
+                                Icons.star,
+                                color: const Color.fromARGB(255, 26, 182, 107),
+                              ),
+                              onRatingUpdate: (double value) {},
+                            ),
+                          ),
+                          Divider(color: Colors.grey),
+                          ListTile(
+                            title: Text(
+                              "가게 3",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w700, fontSize: 22),
+                            ),
+                            trailing: Container(
+                              alignment: Alignment.bottomRight,
+                              width: 150,
+                              height: 150,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage('images/store4.jpeg'),
+                                ),
+                              ),
+                            ),
+                            subtitle: RatingBar.builder(
+                              itemSize: 15,
+                              initialRating: 4,
+                              minRating: 1,
+                              direction: Axis.horizontal,
+                              allowHalfRating: true,
+                              itemCount: 5,
+                              itemBuilder: (context, _) => Icon(
+                                Icons.star,
+                                color: const Color.fromARGB(255, 26, 182, 107),
+                              ),
+                              onRatingUpdate: (double value) {},
+                            ),
+                          ),
+                          Divider(color: Colors.grey),
+                          ListTile(
+                            title: Text(
+                              "가게 4",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w700, fontSize: 22),
+                            ),
+                            trailing: Container(
+                              alignment: Alignment.bottomRight,
+                              width: 150,
+                              height: 150,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage('images/store5.jpeg'),
+                                ),
+                              ),
+                            ),
+                            subtitle: RatingBar.builder(
+                              itemSize: 15,
+                              initialRating: 5,
+                              minRating: 1,
+                              direction: Axis.horizontal,
+                              allowHalfRating: true,
+                              itemCount: 5,
+                              itemBuilder: (context, _) => Icon(
+                                Icons.star,
+                                color: const Color.fromARGB(255, 26, 182, 107),
+                              ),
+                              onRatingUpdate: (double value) {},
+                            ),
+                          ),
+                          Divider(
+                              color: const Color.fromARGB(255, 123, 123, 123)),
+                        ],
+                      ),
+                      Positioned(
+                        top: 15,
+                        left: 170,
+                        right: 170,
+                        child: Container(
+                          height: 4,
+                          width: 5,
+                          decoration: BoxDecoration(
+                            color: Colors.grey,
+                            borderRadius: BorderRadius.circular(2),
+                          ),
                         ),
-
-                        // Declare your sheet content widgets ahead
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 );
               },
--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       // home: SelectScreen(),
-      home: MydataScreen(),
+      home: SelectScreen(),
       getPages: [
         GetPage(
           name: '/Map',
